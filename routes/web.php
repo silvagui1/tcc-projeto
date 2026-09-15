bb7aab1113bda6d884d25d2180399d808de7ce70
casdean <meduarda6660@gmail.com>
2026-09-15 18:23:22 -0300

    tsteee

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use App\Http\Middleware\LogAcessoMiddleware;
 Route::get('/', [App\Http\Controllers\Principal::class, 'principal']);
 
 
+
+
 Route::prefix('/aluno')->group(function(){
     Route::get('/index', [App\Http\Controllers\AlunoController::class, 'index'])->name('aluno.index');
     Route::post('/add', [App\Http\Controllers\AlunoController::class, 'add'])->name('aluno.add');
